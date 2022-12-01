9ec889942e93ce2827bc06f16af8c12b61c2ff31
emil-westling <westling01@gmail.com>
2022-12-01 12:00:49 -0500

    some methods in angleLinkedEquivalence

--- src/geometry_objects/angle/AngleLinkedEquivalenceClass.java
+++ src/geometry_objects/angle/AngleLinkedEquivalenceClass.java
@@ -25,5 +25,45 @@ import utilities.eq_classes.LinkedEquivalenceClass;
  */
 public class AngleLinkedEquivalenceClass extends utilities.LinkedEquivalenceClass<Angle>
 {
-    // TODO
+    public AngleLinkedEquivalenceClass() {
+    	super(new AngleStructureComparator());
+    }
+    
+    /**
+     * angles with the same vertex and overlapping rays are equivalent
+     * 
+     * @param a
+     * @return true if a belongs in equivalence class
+     */
+    @Override
+    public boolean belongs(Angle a) {
+    	if (a == null) return false;
+    	return (_comparator.compare(a, _canonical) == 1 || _comparator.compare(a, _canonical) == -1
+    			|| _comparator.compare(a, _canonical) == 0);
+    }
+    
+    @Override
+    public void setCanonical(Angle element) {
+    	if (_comparator.compare(element, _canonical) == -1) _canonical = element;
+    }
+    
+    @Override
+    public boolean demoteAndSetCanonical(Angle a) {
+    	// TODO
+    }
+    
+    @Override
+    public String toString() {
+    	// TODO
+    }
+    
+    @Override
+    public boolean contains(Angle a) {
+    	// TODO
+    }
+    
+    @Override
+    public boolean add(Angle a) {
+    	// TODO
+    }
 }
