5e6b149161ba7672dd89ff738cd605c17b1618fc
emil-westling <westling01@gmail.com>
2022-12-05 22:33:58 -0500

    updated test class

--- test/geometry_objects/angle/AngleEquivalenceClassesTest.java
+++ test/geometry_objects/angle/AngleEquivalenceClassesTest.java
@@ -12,7 +12,7 @@ public class AngleEquivalenceClassesTest {
 
 	@Test
 	void addTest() {
-		//  E(0,0)----F(4,0)-----H(8,0) 
+		//  E(0,0)----F(4,0)-----H(8,0)
 		//   \         |         /
 		//    \        |        / 
 		//	  A(2,2)--D(4,2)- B(6,2) 
@@ -24,13 +24,13 @@ public class AngleEquivalenceClassesTest {
 		//     	     \ | /
 		//            C(4,4)
 		
-		Point a = new Point(2,2);
-		Point b = new Point(6,2);
-		Point c = new Point(4,4);
-		Point d = new Point(4,2);
-		Point e = new Point(0,0);
-		Point f = new Point(4,0);
-		Point h = new Point(8,0);
+		Point a = new Point("a", 2,2);
+		Point b = new Point("b", 6,2);
+		Point c = new Point("c", 4,4);
+		Point d = new Point("d", 4,2);
+		Point e = new Point("e", 0,0);
+		Point f = new Point("f", 4,0);
+		Point h = new Point("h", 8,0);
 		Segment ad = new Segment(a, d);
 		Segment ac = new Segment(a, c);
 		Segment bc = new Segment(b, c);
@@ -54,7 +54,7 @@ public class AngleEquivalenceClassesTest {
 			assertTrue(aEqCl.add(acd));
 			assertTrue(aEqCl.add(acb));
 			assertTrue(aEqCl.add(ech));
-			assertEquals(4, aEqCl.size());
+			assertEquals(4, aEqCl.numClasses());
 		}
 		catch (Exception exc) {
 			System.err.println("Angles are invalid");
