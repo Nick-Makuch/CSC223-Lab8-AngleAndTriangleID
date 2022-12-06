08725040761588dbf6051f3522e7b6b8175c118e
emil-westling <westling01@gmail.com>
2022-12-05 23:09:11 -0500

    triangleID

--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -1,6 +1,8 @@
 package preprocessor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -39,23 +41,27 @@ public class TriangleIdentifier
 		return _triangles;
 	}
 
+	
 	private void computeTriangles() throws FactException
 	{
-		// TODO
-		
-		
-		
-//		Set<Segment> segSet = _segments.keySet();
-//		AngleIdentifier angID = new AngleIdentifier(_segments);
-//		AngleEquivalenceClasses angles = angID.getAngles();
-//		
-//		for(AngleLinkedEquivalenceClass alec : angles.getList())
-//		{
-//			List lec = alec.getList();
-//			for(Object angle : lec) 
-//			{
-//				//if(segSet.contains(new Segment(angle, null)))
-//			}
-//		}
+		Collection<Segment> collec = _segments.values();
+		List<Segment> ls = new ArrayList<Segment>(collec);
+		// get all different combinations of segments with a triple nested for-loop
+		for (Segment s1 : ls) {
+			for (Segment s2 : ls) {
+				for (Segment s3 : ls) {
+					// create list that is passed into triangle constructor
+					List<Segment> inList = new ArrayList<Segment>();
+					inList.add(s1);
+					inList.add(s2);
+					inList.add(s3);
+					// try to create triangle with given segments
+					try {
+						_triangles.add(new Triangle(inList));
+					} catch (FactException e) {
+					}
+				}
+			}
+		}
 	}
 }
